7c2c33562fea6a4b8cac923288f392461a4e235a
bora <boraoren@gmail.com>
2020-11-02 10:39:14 +1100

    feat(components/table): #3 has cell component

--- src/components/table/Table.test.tsx
+++ src/components/table/Table.test.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import {shallow} from "enzyme";
+import {shallow, ShallowWrapper} from "enzyme";
 import {Table} from "./index";
 import {Text} from "../text";
+import {Cell} from "../cell";
 
 describe("<Table> component", () => {
 
+    let wrapper:ShallowWrapper;
+
+    beforeAll(()=>{
+       wrapper = shallow((
+           <Table/>
+       ));
+    })
+
     test(`has <Text/> component`, () => {
+        expect(wrapper.contains(<Text/>)).toEqual(true);
+    });
+
+    test(`has <Cell/> component`, () => {
         const wrapper = shallow((
             <Table/>
         ));
 
-        expect(wrapper.contains(<Text/>)).toEqual(true);
+        expect(wrapper.contains(<Cell/>)).toEqual(true);
     });
+
 
 });
--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text} from "../text";
+import {Cell} from "../cell";
 
 export const Table = () => {
     return (
         <>
             <Text/>
+            <Cell/>
         </>
     )
 }
