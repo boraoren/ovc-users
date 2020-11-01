7eb3f0f89a80cffa8045a8e8eacf69b1e491424f
bora <boraoren@gmail.com>
2020-11-02 10:05:09 +1100

    fix(storybook): configure jest-test-results.json file path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,21 @@
-
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-jest"
-  ]
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-jest"
+    ],
+    webpackFinal: (webpackConfig) => {
+        const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+            ({constructor}) =>
+                constructor && constructor.name === 'ModuleScopePlugin',
+        )
+
+        webpackConfig.resolve.plugins.splice(scopePluginIndex, 1)
+        return webpackConfig
+    }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import {addDecorator} from "@storybook/react";
 import {withTests} from "@storybook/addon-jest";
-import results from '../src/.jest-test-results.json';
+import results from '../.jest-test-results.json';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
