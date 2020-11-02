b43075f9f295d4527a5e9126eda0d88e2a008d45
bora <boraoren@gmail.com>
2020-11-02 18:11:02 +1100

    feat(components/table): #7 table with header for name and email

--- src/components/table/TableWithHeader.test.tsx
+++ src/components/table/TableWithHeader.test.tsx
@@ -4,23 +4,51 @@ import {Header} from "../header";
 import React from "react";
 import {User} from "../../models/User";
 
-const users: User[] = [{
-    name: "Leanne Graham",
-}]
+describe("<Table> component", () => {
 
+    test(`with <Header/> for name`,
+        () => {
 
-test(`with <Header/> for name`,
-    () => {
-        render(<Table
-            users={users}
-            header={
-                <Header
-                    dataTestId={"headerDataTestId"}
-                    object={users[0]}/>
-            }/>)
+            const users: User[] = [{
+                name: "Leanne Graham",
+            }]
 
-        const headerComponent = screen
-            .getByTestId("headerDataTestId");
-        expect(headerComponent).toBeTruthy();
-        expect(headerComponent).toHaveTextContent("NAME")
-    })
+            render(<Table
+                users={users}
+                header={
+                    <Header
+                        dataTestId={"headerDataTestId"}
+                        object={users[0]}/>
+                }/>)
+
+            const headerComponent = screen
+                .getByTestId("headerDataTestId");
+            expect(headerComponent).toBeTruthy();
+            expect(headerComponent).toHaveTextContent("NAME");
+        })
+
+    test(`with <Header/> for name and email`,
+        () => {
+
+            const users: User[] = [{
+                name: "Leanne Graham",
+                email: "Sincere@april.biz",
+            }]
+
+            render(<Table
+                users={users}
+                header={
+                    <Header
+                        dataTestId={"headerDataTestId"}
+                        object={users[0]}/>
+                }/>)
+
+            const headerComponent = screen
+                .getByTestId("headerDataTestId");
+            expect(headerComponent).toBeTruthy();
+            expect(headerComponent).toHaveTextContent("NAME");
+            expect(headerComponent).toHaveTextContent("EMAIL");
+
+        });
+
+});
