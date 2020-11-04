91fc65c3a2004bc8b2c785a442e184419ebd9496
bora <boraoren@gmail.com>
2020-11-04 16:41:42 +1100

    feature(components/table) #21 add test for user details

--- src/components/table/Table.test.tsx
+++ src/components/table/Table.test.tsx
@@ -5,6 +5,7 @@ import {Text} from "../text";
 import {Cell} from "../cell";
 import {render, screen} from "@testing-library/react";
 import {User} from "../../models/User";
+import {UserDetail} from "../../models/UserDetail";
 
 describe("<Table> component", () => {
 
@@ -133,5 +134,30 @@ describe("<Table> component", () => {
             })
 
         });
+
+    test(`<Table/> for user details`,
+        () => {
+
+            const userDetails = [{
+                id: 1,
+                title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                body: "quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto",
+            }, {
+                id: 2,
+                title: "qui est esse",
+                body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+            }] as UserDetail[];
+
+
+            render(<Table data={userDetails}/>)
+
+            const cellComponent = screen.getAllByTestId("titleCellDataTestId");
+
+            cellComponent.forEach((cell, index)=>{
+                expect(cell.textContent).toContain(userDetails[index].title);
+            })
+
+        });
+
 
 });
